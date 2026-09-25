6418161ac145a4edbad9fc15b64b44f7e30002c2
Ahmed Shazly <ahmedshazlyabouelelaf@gmail.com>
2026-09-25 21:03:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -274,6 +274,6 @@ if submitted:
 st.divider()
 st.markdown(
     '<div class="footer-note"><b>Flight Delay Intelligence</b> · Machine Learning Project · '
-    '2015 U.S. Domestic Flights<br>Academic prediction system — not live airline operational advice.</div>',
+    '2015 U.S. Domestic Flights<br>Academic prediction system — not live airline operational advice , This Dashboard Created by Ahmed Shazly.</div>',
     unsafe_allow_html=True,
 )
